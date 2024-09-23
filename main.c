8f6a25011d0c097dbfa019c99c64dea5d23bdedb
tonegi <90652377+tonegi@users.noreply.github.com>
2024-09-23 17:01:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void) {
 double number1, number2, mean;  
-  printf("Type your 1st semester grade: ");
+  printf("Enter your 1st semester grade: ");
       scanf("%lf",&number1);
-  printf("Type your 2nd semester grade: ");
+  printf("Enter your 2nd semester grade: ");
       scanf("%lf",&number2);
     mean = (number1 + number2)/2;
    printf("Your final mean is: %.2f", mean); 
